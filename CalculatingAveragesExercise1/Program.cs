70bcf159f34aebae686d7a08fd2bbbe04e927696
Samuel Lane <samuel.lane.sa@outlook.com>
2019-03-03 12:41:15 -0500

    CalculatingAveravesEx1

--- CalculatingAveragesExercise1/Program.cs
+++ CalculatingAveragesExercise1/Program.cs
@@ -10,6 +10,50 @@ namespace CalculatingAveragesExercise1
     {
         static void Main(string[] args)
         {
+            double numOne, numTwo, numThree, numFour, numFive, numSix, numSeven, numEight, numNine, numTen;
+            double sumOfNumbers;
+            double averageNumbers;
+
+
+            Console.WriteLine($"Please enter your first Number. ");
+            numOne = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine($"Please enter your Second Number. ");
+            numTwo = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine($"Please enter your Third Number. ");
+            numThree = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine($"Please enter your Fourth Number. ");
+            numFour = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine($"Please enter your Fifth Number. ");
+            numFive = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine($"Please enter your Sixth Number. ");
+            numSix = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine($"Please enter your Seventh Number. ");
+            numSeven = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine($"Please enter your Eighth Number. ");
+            numEight = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine($"Please enter your Ninth Number. ");
+            numNine = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine($"Please enter your Tenth Number. ");
+            numTen = Convert.ToDouble(Console.ReadLine());
+
+            sumOfNumbers = numOne + numTwo + numThree + numFour + numFive + numSix + numSeven + numEight + numNine + numTen;
+
+            averageNumbers = sumOfNumbers / 10;
+
+            Console.WriteLine($"The Sum of your numbers is : {sumOfNumbers}. ");
+
+            Console.WriteLine($"The Average of your numbers is : {averageNumbers}. ");
         }
+
+
     }
 }
